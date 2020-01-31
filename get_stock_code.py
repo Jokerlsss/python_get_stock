36cb0b0824e37d0e9fe375a76a68a8d80657df4d
Jokerls <1152501640@qq.com>
2020-01-31 22:40:31 +0800

    2020-1-31 get the stock_code

---
+++ get_stock_code.py
@@ -0,0 +1,28 @@
+import requests
+import random
+from bs4 import BeautifulSoup as bs
+import time
+import redis
+
+# 2020-1-31：未解决问题：连接不上redis
+def get_stock_names():
+    # rds=redis.Redis(host="175.24.66.216", port=6379, password="123456")
+    # rds = redis.from_url('redis://:123456@175.24.66.216:6379/0',db=1,decode_responses=True)
+    url = "http://quote.eastmoney.com/stock_list.html#"
+    headers = {
+        'Referer':'http://quote.eastmoney.com',
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+    }
+
+    response = requests.get(url,headers=headers).content.decode('gbk')
+    soup = bs(response,'lxml')
+    all_ul=soup.find('div',id='quotesearch').find_all('ul')
+    print('all_ul',all_ul)
+    with open('stock_names.txt','w+',encoding='utf-8') as f:
+        for ul in all_ul:
+            all_a=ul.find_all('a')
+            for a in all_a:
+                # rds.rpush('stock_names',a.text)
+                f.write(a.text+'\n')
+
+get_stock_names()
