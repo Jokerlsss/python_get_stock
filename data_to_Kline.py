65eb6878df85becd0eaaf4d78bcb3051271db1ad
Jokerls <1152501640@qq.com>
2020-02-10 00:10:47 +0800

    2020-2-10 finish api to get data

--- data_to_Kline.py
+++ data_to_Kline.py
@@ -3,9 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import pandas as pd
 import random
+import json
 import plotly.offline as po
 import plotly.graph_objs as go
+import time
 
+from flask_cors import *
+from flask import Flask,request
+app=Flask(__name__)
+
+@app.route('/get',methods=['GET'])
 def get_graph():
     # 连接 mysql
     engine = create_engine('mysql+mysqlconnector://root:123456@localhost:3306/stock_data')
@@ -21,20 +28,46 @@ def get_graph():
     # 随机选取一只股票
     stock_name=random.choice(tables_names)
     # 获取股票数据
+    print(stock_name)
     df=pd.read_sql('select * from `{}`'.format(stock_name),con=con)
 
-    trace=go.Candlestick(x=df['日期'],
-                         open=df['开盘价'],
-                         high=df['最高价'],
-                         low=df['最低价'],
-                         close=df['收盘价'])
+    # to_json 将数据类型以 values 形式转换为 json 格式
+    jsonDF=df.to_json(orient="values",force_ascii=False,date_format="日期")
+    # json.loads 将字符串转化为 list
+    # json.dumps 将 list 转化为字符串
+    jsonToList=json.loads(jsonDF)
 
-    data=[trace]
-    layout={
-        'title':stock_name
-    }
-    fig=dict(data=data,layout=layout)
-    po.plot(fig,filename='stock.html')
+    # para用于最终的数据格式呈现，如：[["xxx","yy"],["xx","yy"]]
+    para = []
+    for i in jsonToList:
+        # 时间戳多出3个0，为 int 类型，故处理成 除以1000
+        timeArray=time.localtime(i[0]/1000)
+        # otherTime 为日期格式呈现的数据
+        otherTime=time.strftime("%Y-%m-%d",timeArray)
+        # 将日期格式的数据替换掉数组中的时间戳
+        i[0]=otherTime
+
+        # 将类型为 int 的列表，转化为 str，如 [2019-01-01,24] -> ['2019-01-01','24']
+        b=[str(j) for j in i]
+        para.append(b)
+    # list 不能作为返回值，得先转成 json格式
+    return json.dumps(para,ensure_ascii=False)
+    print("para:",para)
 
 
-get_graph()
+    # ------------------------ 将 K 线图画在 html 文件中----------------------
+    # trace=go.Candlestick(x=df['日期'],
+    #                      open=df['开盘价'],
+    #                      high=df['最高价'],
+    #                      low=df['最低价'],
+    #                      close=df['收盘价'])
+    #
+    # data=[trace]
+    # layout={
+    #     'title':stock_name
+    # }
+    # fig=dict(data=data,layout=layout)
+    # po.plot(fig,filename='stock.html')
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5590)
---
+++ readMysql_stock.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column,Integer,String
+
+from sqlalchemy.orm import sessionmaker
+
+# --------------------------------- 连接 mysql  ---------------------------------
+
+def getcontent():
+    # 参数配置
+    engine = create_engine('mysql+mysqlconnector://root:123456@localhost:3306/stock_data')
+    # 建立连接
+    con = engine.connect()
+
+    # 创建 session 类型
+    DBSession =sessionmaker(bind=engine)
+    # 创建 session 对象
+    session=DBSession()
+
+    stock_info=session.query()
+
+if __name__ == '__main__':
+    getcontent()
